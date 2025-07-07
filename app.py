223fd830346fb765be11cfeb4da6419ea72c5799
Lawrence <l.laguidao.533308@umindanao.edu.ph>
2025-07-07 15:33:29 +0800

    modified pkl s

--- app.py
+++ app.py
@@ -9,16 +9,11 @@ import time
 # ----------------------------
 # Load model from Google Drive
 # ----------------------------
+
 st.set_page_config(page_title="Heart Disease Risk Checker", layout="centered")
 model_path = "heart_disease_model.pkl"
 
-# first pkl
-# file_id = "1_OqUNI5f3q_BgvjtNnepRIebqgg_6VHR"
-
-# second pkl
-# file_id = "1X8hT8aXCKWVzNmPzAaZ3va6ByyS0tbvd"
-
-# third pkl
+# model pkl
 file_id = "1MO7WGYVXLN5dQ6LmonGJ5HKfUqtSNVU6"
 
 url = f"https://drive.google.com/uc?id={file_id}"
